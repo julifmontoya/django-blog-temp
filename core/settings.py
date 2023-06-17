f9b8a1afd658c272930920e9c3b43b603ef4a84c
Felipe Montoya <julifmontoya@hotmail.com>
2023-06-17 13:05:02 -0500

    ico

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,13 @@ LOGIN_URL = 'login'
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+""" STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') """
+
+
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUD_NAME'),
